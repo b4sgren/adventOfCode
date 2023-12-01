0861fd80fb818b95c4c13ab9a91fe4d58c5c7a46
Brendon Forsgren <realpepino@gmail.com>
2023-12-01 08:44:05 -0600

    Still stuck. One last shot but need to wait to test it

--- 2023/day1.py
+++ 2023/day1.py
@@ -1,5 +1,6 @@
 import sys
 import numpy as np
+from copy import deepcopy as copy
 
 def part1():
     with open('day1.txt', 'r') as f:
@@ -28,25 +29,41 @@ def part2():
 
     vals = []
 
-    nums_text = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    nums_text = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']  #, '1', '2', '3', '4', '5', '6', '7', '8', '9']
     nums_digit_map = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
     for str in data:
+        str_orig = copy(str)
         num = ''
         idxs = []
-        for temp in nums_text:
+        for i, temp in enumerate(nums_text):
             idxs.append(str.find(temp))
+            if idxs[-1] > -1:
+                str = str[:idxs[-1]] + nums_digit_map[i] + str[idxs[-1]+1:]
 
-        max_id = np.argmax(idxs)
-        min_id = -1
-        min_val = 1000
-        for i, id in enumerate(idxs):
-            if id > -1 and id < min_val:
-                min_id = i
-                min_val = id
-        num += nums_digit_map[min_id]
-        if max_id != min_id:
-            num += nums_digit_map[max_id]
+        for char in str:
+            if char.isdigit():
+                num += char
+                break
+        for char in reversed(str):
+            if char.isdigit():
+                num += char
+                break
+
+        # max_id = np.argmax(idxs)
+        # max_val = np.max(idxs)
+        # min_id = -1
+        # min_val = 1000
+        # for i, id in enumerate(idxs):
+        #     if id > -1 and id < min_val:
+        #         min_id = i
+        #         min_val = id
+        # num += nums_digit_map[min_id]
+        # num += nums_digit_map[max_id]
+        # if max_val < min_val or max_val == min_val:
+        #     debug = 1
+        # # if max_id != min_id:
+        # #     num += nums_digit_map[max_id]
 
         vals.append(int(num))
 
