f2717d954e3d6a3cca9a059ad17b5f34e27be8f4
Brendon Forsgren <realpepino@gmail.com>
2023-12-01 23:28:48 -0600

    Got part 1 done

---
+++ 2023/day2.py
@@ -0,0 +1,48 @@
+import sys
+import numpy as np
+
+def part1():
+    with open('day2.txt', 'r') as f:
+    # with open('temp.txt', 'r') as f:
+        data = f.readlines()
+
+    colors = ['red', 'blue', 'green']
+    max_cubes_dict = {'red':12, 'green':13, 'blue':14}
+
+    game_ids = []
+
+    for z, line in enumerate(data):
+        # look for number of cubes of each color
+        # find number and see if correct
+        tokens = line.split()
+        game_id = tokens[1].split(':')[0]
+        tokens = tokens[2:]
+
+        is_possible = True
+
+        for i in range(0, len(tokens), 2):
+            num_cubes = int(tokens[i])
+            for temp in colors:
+                if tokens[i+1].find(temp) > -1:
+                    color = temp
+                    break
+
+            if num_cubes > max_cubes_dict[color]:
+                is_possible = False
+                break
+                debug = 1
+
+        if is_possible:
+            game_ids.append(int(game_id))
+
+
+    print(np.sum(game_ids))
+
+def part2():
+    # with open('day2.txt', 'r') as f:
+    with open('temp.txt', 'r') as f:
+        data = f.readlines()
+
+if __name__=="__main__":
+    part1()
+    # part2()
