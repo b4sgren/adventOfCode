007dcb58f16eee1613bf9b6681b0f3fd3a9ed333
Brendon Forsgren <realpepino@gmail.com>
2023-12-03 10:01:47 -0600

    Some edge case is not working. Think about different parsing errors

--- 2023/day3.py
+++ 2023/day3.py
@@ -3,6 +3,11 @@ import numpy as np
 def is_symbol(c):
     return (not c.isdigit() and not c == '.')
 
+def printLines(prev, curr, next):
+    print(prev)
+    print(curr)
+    print(next)
+
 def getNumbersInLine(line):
     numbers = []
     symbols = []
@@ -27,7 +32,7 @@ def check_line(idx, size, line):
     if idx > 0:
         b1 = is_symbol(line[idx-1])
     if idx < len(line):
-        b2 = is_symbol(line[idx + size + 1])
+        b2 = is_symbol(line[idx + size])
     if b1 or b2:
         return True
 
@@ -55,8 +60,9 @@ def is_part_num(idx, size, prev_line, line, next_line):
     return False
 
 def part1():
-    # with open('day3.txt', 'r') as f:
-    with open('temp.txt', 'r') as f:
+    with open('day3.txt', 'r') as f:
+    # with open('temp.txt', 'r') as f:  # answer is 4361
+    # with open('temp2.txt', 'r') as f:  # answer is 925
         data = f.readlines()
         max_lines = len(data)
 
@@ -64,13 +70,36 @@ def part1():
     next_line = data[1]
     part_nums = []
     for i, line in enumerate(data):
+        curr_nums = []
         numbers, symbols = getNumbersInLine(line)
+        debug = 1
         for num in numbers:
             idx = line.find(num)
-            if is_part_num(idx, len(num), prev_line, line, next_line):
+            size = len(num)
+
+            if is_part_num(idx, size, prev_line, line, next_line):
                 part_nums.append(int(num))
+                curr_nums.append(int(num))
+            line = line[:idx] + "."*size + line[idx+size:]
+
+        debug = 1
+        printLines(prev_line, data[i], next_line)
+        print(curr_nums)
+        print("=======================================\n")
+        prev_line = data[i]
+        if i + 2 < len(data):
+            next_line = data[i+2]
+        else:
+            next_line = None
 
     print(np.sum(part_nums))
+
+'''
+Part 1 notes:
+532672 is to high
+531496 is to high
+531491 is to high
+'''
 
 
 if __name__=="__main__":
