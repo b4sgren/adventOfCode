2837cf1594d7d0222e8b117df24251c235c62cc8
Brendon Forsgren <realpepino@gmail.com>
2023-12-05 20:54:18 -0600

    Figured out part 1! Wasn't too bad

--- 2023/day5.py
+++ 2023/day5.py
@@ -3,7 +3,8 @@ import numpy as np
 def parseSection(data, line_num):
     line_num += 1
     line = data[line_num].split()
-    src_dest_map = {}
+    src_vals = []
+    dest_vals = []
     flag = True
 
     while(len(line) == 3 and flag):
@@ -11,9 +12,8 @@ def parseSection(data, line_num):
         src_num = int(line[1])
         range_num = int(line[2])
 
-        # Enter values here
-        for i in range(range_num):
-            src_dest_map[src_num+i] = dest_num + i
+        src_vals.append([src_num, src_num + range_num - 1])
+        dest_vals.append([dest_num, dest_num + range_num - 1])
 
         line_num += 1
         if line_num < len(data):
@@ -22,7 +22,7 @@ def parseSection(data, line_num):
             flag = False
 
     line_num += 1
-    return src_dest_map, line_num
+    return src_vals, dest_vals, line_num
 
 
 def parseFile(data):
@@ -55,27 +55,33 @@ def parseFile(data):
     return seeds, seed_soil_map, soil_fertilizer_map, fertilizer_water_map, water_light_map, light_temperature_map, temperature_humidity_map, humidity_location_map
 
 
-def getValueFromMap(val, map):
-    if val in map.keys():
-        ans = map[val]
-    else:
+def getValueFromMap(val, srcs, dest):
+    found_val = False
+    for i, src in enumerate(srcs):
+        if val >= src[0] and val <= src[1]:
+            found_val = True
+            dist = val - src[0]
+            ans = dest[i][0] + dist
+            break
+
+    if found_val is False:
         ans = val
 
     return ans
 
 def getValue(src_list, data, line_num):
-    map, line_num = parseSection(data, line_num)
+    src_vals, dest_vals, line_num = parseSection(data, line_num)
 
     dest_list = []
     for val in src_list:
-        dest_list.append(getValueFromMap(val, map))
+        dest_list.append(getValueFromMap(val, src_vals, dest_vals))
 
     return dest_list, line_num
 
 
 def part1():
-    with open('temp.txt', 'r') as f:
-    # with open('day5.txt', 'r') as f:
+    # with open('temp.txt', 'r') as f:
+    with open('day5.txt', 'r') as f:
         data = f.readlines()
     # seeds, seed_soil_map, soil_fertilizer_map, fertilizer_water_map, water_light_map, light_temperature_map, temperature_humidity_map, humidity_location_map = parseFile(data)
 
