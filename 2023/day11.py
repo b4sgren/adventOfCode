f376b686672075f6b2b774d170a06b57f31be19e
Brendon Forsgren <realpepino@gmail.com>
2023-12-11 07:43:48 -0600

    Fixed one small bug in part 2. Closer but still not quite right on test inputs

--- 2023/day11.py
+++ 2023/day11.py
@@ -68,13 +68,89 @@ def part1():
 
     print(np.sum(list(path_length_dict.values())))
 
+def parseData2(data):
+    graph = []
+    rows_with_galaxies = [False for _ in range(len(data))]
+    cols_with_galaxies = [False for _ in range(len(data[0]))]
+    counter = 0
+    for i, line in enumerate(data):
+        vals = list(line)[:-1]
+        while '#' in vals:
+            rows_with_galaxies[i] = True
+            idx = vals.index('#')
+            cols_with_galaxies[idx] = True
+            vals[idx] = str(counter)
+            counter += 1
+        graph.append(vals)
 
+    # Append space where no galaxies are in a row/col
+    offset = 0
+    for i, val in enumerate(rows_with_galaxies):
+        if not val:
+            graph.insert(i + offset, ['1000000']*len(graph[0]))
+            offset += 1
+
+    offset = 0
+    for i, val in enumerate(cols_with_galaxies):
+        if not val:
+            for j in range(len(graph)):
+                graph[j].insert(i+offset, '1000000')
+            offset += 1
+
+    return graph, counter
 
 
 def part2():
-    pass
+    with open('temp.txt', 'r') as f:
+    # with open('input.txt', 'r') as f:
+        data = f.readlines()
+
+    graph, num_galaxies = parseData2(data)
+
+    paths_dict = {}
+    for i in range(num_galaxies-1):
+        for j in range(i+1,num_galaxies):
+            paths_dict[(i, j)] = []
+
+    locations_dict = {}
+    # Can just use addition and subtraction for apsp
+    for i in range(num_galaxies):
+        for j, line in enumerate(graph):
+            if str(i) not in line:
+                continue
+            idx = line.index(str(i))
+            locations_dict[i] = [j, idx]
+            break
+
+    path_length_dict = {}
+    for key, loc in locations_dict.items():
+        for key2, loc2 in locations_dict.items():
+            if key2 <= key:
+                continue
+            num_expanded_cols = 0
+            for i in range(loc[1], loc2[1]):
+                if graph[0][i] == '1000000':
+                    num_expanded_cols += 1
+
+            num_expanded_rows = 0
+            for i in range(loc[0], loc2[0]):
+                if len(set(graph[i])) == 1 and graph[i][0] == '1000000':
+                    num_expanded_rows += 1
+
+            # temp = 1000000 - 1
+            temp = 10  # 1030 on test input
+            # temp = 100  # 8410 on test input
+            path_length = abs(loc[0] - loc2[0]) + abs(loc[1] - loc2[1]) + temp * (num_expanded_cols + num_expanded_rows)
+            path_key = [key, key2]
+            path_key.sort()
+            if path_key == [0, 1]:
+                debug = 1
+            path_length_dict[tuple(path_key)] = path_length
+
+    print(np.sum(list(path_length_dict.values())))
+
 
 if __name__=="__main__":
-    part1()
+    # part1()
 
     part2()
