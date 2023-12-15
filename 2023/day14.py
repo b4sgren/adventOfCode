29db58d4bef6082530745ccbcb8c6765dd08fd41
Brendon Forsgren <realpepino@gmail.com>
2023-12-14 20:09:38 -0600

    Part 1 is working for test input

--- 2023/day14.py
+++ 2023/day14.py
@@ -1,9 +1,41 @@
 
+def parseData(data):
+    map = []
+    for line in data:
+        map.append(list(line)[:-1])
+
+    return map
+
 def part1():
-    pass
+    with open("temp2.txt", 'r') as f:
+        data = f.readlines()
+    map = parseData(data)
+
+    num_cols = len(map[0])
+    num_rows = len(map)
+    for i in range(num_cols):
+        for j in range(0, num_rows):
+            if map[j][i] != 'O':
+                continue
+            for k in range(j, 0, -1):
+                if map[k-1][i] == '#' or map[k-1][i] == 'O':
+                    break
+                map[k-1][i] = 'O'
+                map[k][i] = '.'
+
+    weight = 0
+    for i, line in enumerate(map):
+        cnt = line.count('O')
+        factor = num_rows - i
+        weight += cnt * factor
+
+    print(weight)
+
 
 def part2():
     pass
 
 if __name__=="__main__":
-    pass
+    part1()
+
+    part2()
