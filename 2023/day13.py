0fe9f35f36c9145508a78335920dd7cc4370081a
Brendon Forsgren <realpepino@gmail.com>
2023-12-14 22:09:02 -0600

    Working on logic to identify the key location that needs to change

--- 2023/day13.py
+++ 2023/day13.py
@@ -70,6 +70,67 @@ def part1():
 
     print(count)
 
+def identifyKeyLocation(pattern):
+    count = 0
+
+    num_cols = len(pattern[0])
+    # Check symmetry across a vertical line
+    for line in range(1, num_cols):
+        is_symmetric = True
+        diffs = 0
+        ids = []
+        for r, row in enumerate(pattern):
+            first_half = row[:line]
+            first_half.reverse()
+            second_half = row[line:]
+            s1 = len(first_half)
+            s2 = len(second_half)
+            size = min(s1, s2)
+            diff = [n1==n2 for n1,n2 in zip(first_half, second_half)]
+            if diff.count(True) == 1:
+                idx = diff.index(True)
+                ids.append([r, line])
+            diffs += diff.count(True)
+            if diffs > 1:
+                is_symmetric = False
+                break
+
+        if is_symmetric:
+            count += line
+            break
+
+    if len(ids) == 1:
+        return ids[0]
+
+
+    num_rows = len(pattern)
+    for line in range(1, num_rows):
+        top_half = pattern[:line]
+        top_half.reverse()
+        bottom_half = pattern[line:]
+        s1 = len(top_half)
+        s2 = len(bottom_half)
+        size = min(s1, s2)
+
+        is_symmetric = True
+        diffs = 0
+        ids = []
+        for i in range(size):
+            diff = [n1==n2 for n1,n2 in zip(top_half[i], bottom_half[i])]
+            if diff.count(True) == 1:
+                idx = diff.index(True)
+                ids.append([line+i, idx])  # not right. Either top_half[i] or bottom half[i]
+            diffs += diff.count(True)
+            if diffs > 1:
+                is_symmetric = False
+                break
+
+        if is_symmetric:
+            count += 100*line
+
+    return ids[0]
+
+
 def part2():
     # with open('temp.txt', 'r') as f:
     with open('input.txt', 'r') as f:
@@ -79,7 +140,7 @@ def part2():
 
     count = 0
     for pattern in patterns:
-        count += findLineOfSymmetry(pattern)
+        count += identifyKeyLocation(pattern)
 
     print(count)
 
