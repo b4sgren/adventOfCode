6378819c7aba2ff1a869aa2ed5806584e5953a92
Brendon Forsgren <realpepino@gmail.com>
2023-12-15 20:06:42 -0600

    Added parsing for day15

--- 2023/day15.py
+++ 2023/day15.py
@@ -1,6 +1,17 @@
 
+def parseData(data):
+    vals = data.split(',')
+    strings = [list(v) for v in vals]
+    strings[-1] = strings[-1][:-1]
+
+    return strings
+
 def part1():
-    pass
+    with open('temp2.txt', 'r') as f:
+        data = f.readlines()
+
+    strings = parseData(data[0])
+    debug = 1
 
 def part2():
     pass
