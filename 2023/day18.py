b82f04a9c5c2c843a47b1324c51dbad132120b4f
Brendon Forsgren <realpepino@gmail.com>
2023-12-18 07:18:26 -0600

    Added parser for day18. This day sounds more doable

---
+++ 2023/day18.py
@@ -0,0 +1,24 @@
+
+def parseData(data):
+    directions, distances, colors = [], [], []
+    for line in data:
+        vals = line[:-1].split()
+        directions.append(vals[0])
+        distances.append(int(vals[1]))
+        colors.append(vals[2][1:-1])
+
+    return directions, distances, colors
+
+def part1():
+    with open('temp2.txt', 'r') as f:
+        data = f.readlines()
+
+    directions, distances, colors = parseData(data)
+
+def part2():
+    pass
+
+if __name__=="__main__":
+    part1()
+
+    part2()
