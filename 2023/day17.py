d7e95fd78937b0c8ff42445be40dc3afddd7d7d1
Brendon Forsgren <realpepino@gmail.com>
2023-12-17 18:43:16 -0600

    Other strategy is running but not working yet

--- 2023/day17.py
+++ 2023/day17.py
@@ -68,41 +68,16 @@ def findShortestPath(grid, source, target):
                 debug = 1
 
     # Get path
-    path = [target]
-    node = target
-    while tuple(node) in prev_node:
-        node = list(prev_node[tuple(node)])
+    path = []
+    node = copy(source)
+    while prev_node[node] is not None:
         path.append(node)
+        node = prev_node[node]
+    path.append(node)
     path.reverse()
-
     print(path)
-    for val in path:
-        grid[val[0], val[1]] = 0
-    print(grid)
 
     return grid_distances[source]
-
-'''
-1  function Dijkstra(Graph, source):
- 2
- 3      for each vertex v in Graph.Vertices:
- 4          dist[v] ← INFINITY
- 5          prev[v] ← UNDEFINED
- 6          add v to Q
- 7      dist[source] ← 0
- 8
- 9      while Q is not empty:
-10#           u ← vertex in Q with min dist[u]
-11          remove u from Q
-12
-13          for each neighbor v of u still in Q:
-14              alt ← dist[u] + Graph.Edges(u, v)
-15              if alt < dist[v]:
-16                  dist[v] ← alt
-17                  prev[v] ← u
-18
-19      return dist[], prev[]
-'''
 
 def part1():
     with open('temp.txt', 'r') as f:
