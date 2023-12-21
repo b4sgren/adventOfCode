702c102f2d56567d8c56cb28b0e80f8166ed8fab
Brendon Forsgren <realpepino@gmail.com>
2023-12-21 07:15:18 -0600

    Started making edits for part 2. i think I have the wrapping issue solved. Next I need to detect when I'm in a steady state and determine how many tiles I cover

--- 2023/day21.py
+++ 2023/day21.py
@@ -51,9 +51,46 @@ def part1():
 
 
 def part2():
-    pass
+    with open('temp.txt', 'r') as f:
+    # with open('input.txt', 'r') as f:
+        data = f.readlines()
+
+    grid , start_id = parseData(data)
+    # Remove the padding for easier wrapping
+    grid = grid[1:-1]
+    grid = [line[1:-1] for line in grid]
+    start_id = [start_id[0]-1, start_id[1]-1]
+
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+    num_steps = 26501365
+    queue = [tuple(start_id)]  # of of possible current locations
+    for i in range(num_steps):
+        next_queue = []  # queue of possible next locations
+        while len(queue) > 0:
+            location = queue.pop(0)
+            row, col = location
+
+            if grid[(row+1)%num_rows][col] != '#':
+                if ((row+1)%num_rows, col) not in next_queue:
+                    next_queue.append(((row+1)%num_rows, col))
+            if grid[(row-1)%num_rows][col] != '#':
+                if ((row-1)%num_rows, col) not in next_queue:
+                    next_queue.append(((row-1)%num_rows, col))
+            if grid[row][(col+1)%num_cols] != '#':
+                if (row, (col+1)%num_cols) not in next_queue:
+                    next_queue.append((row, (col+1)%num_cols))
+            if grid[row][(col-1)%num_cols] != '#':
+                if (row, (col-1)%num_cols) not in next_queue:
+                    next_queue.append((row, (col-1)%num_cols))
+
+        queue = next_queue
+        next_queue = []
+
+    print(len(queue))
+
 
 if __name__=="__main__":
-    part1()
+    # part1()
 
     part2()
