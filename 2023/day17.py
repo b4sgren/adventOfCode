5559f7103e67a7c07de8e81a5222e0bfce4a1263
Brendon Forsgren <realpepino@gmail.com>
2023-12-21 19:54:09 -0600

    I have no idea how to solve day 17

--- 2023/day17.py
+++ 2023/day17.py
@@ -19,41 +19,75 @@ min_distance = 1e9
 def getNeighbors(source, path):
     neighbors = []
 
+    if len(path) >= 3 and path[0] is tuple:
+        pm3, pm2, pm1 = path[-3:]
+        d1 = pm1[-1]
+        d2 = pm2[-1]
+        d3 = pm3[-1]
+        flag = d1 == d2 == d3
+        direction = d1
+    else:
+        flag = False
+        direction = 0
+
     if source[-1] == 1:
         r, c = source[0]-1, source[1]
-        neighbors.append((r, c, 1))
-        neighbors.append((r, c, 2))
-        neighbors.append((r, c, 4))
+        if r > -1:
+            if not flag and direction != 1:
+                neighbors.append((r, c, 1))
+            if not flag and direction != 2:
+                neighbors.append((r, c, 2))
+            if not flag and direction != 4:
+                neighbors.append((r, c, 4))
     elif source[-1] == 2:
         r, c = source[0], source[1]-1
-        neighbors.append((r, c, 1))
-        neighbors.append((r, c, 2))
-        neighbors.append((r, c, 3))
+        if c > -1:
+            if not flag and direction != 1:
+                neighbors.append((r, c, 1))
+            if not flag and direction != 2:
+                neighbors.append((r, c, 2))
+            if not flag and direction != 4:
+                neighbors.append((r, c, 3))
     elif source[-1] == 3:
         r, c = source[0]+1, source[1]
-        neighbors.append((r, c, 2))
-        neighbors.append((r, c, 3))
-        neighbors.append((r, c, 4))
+        if r < len(grid):
+            if not flag and direction != 2:
+                neighbors.append((r, c, 2))
+            if not flag and direction != 3:
+                neighbors.append((r, c, 3))
+            if not flag and direction != 4:
+                neighbors.append((r, c, 4))
     elif source[-1] == 4:
         r, c = source[0], source[1]+1
-        neighbors.append((r, c, 1))
-        neighbors.append((r, c, 3))
-        neighbors.append((r, c, 4))
+        if c < len(grid[0]):
+            if not flag and direction != 1:
+                neighbors.append((r, c, 1))
+            if not flag and direction != 3:
+                neighbors.append((r, c, 3))
+            if not flag and direction != 4:
+                neighbors.append((r, c, 4))
 
     return neighbors
 
 # Need to check somewhere if the distance is less than min distance
 @cache
 def getShortestPath(source, target, path, distance):
+    global min_distance
     if source[0] == target[0] and source[1] == target[1]:
         if distance < min_distance:
             min_distance = distance
 
     neighbors = getNeighbors(source, path)
     for neighbor in neighbors:
-        new_path = (*path, neighbor)
+        if neighbor in path:
+            continue
+        if path[0] is tuple:
+            new_path = (*path, neighbor)
+        else:
+            new_path = (path, neighbor)
         new_dist = distance + grid[neighbor[0], neighbor[1]]
         getShortestPath(neighbor, target, new_path, new_dist)
+    print(min_distance)
 
 # Try recursively
 def part1():
@@ -71,7 +105,7 @@ def part1():
     source = (0, 0, 3)  # may need to test facing south first
     target = (len(grid)-1, len(grid[0])-1, 1)
     distance = 0
-    path = (source)
+    path = tuple(source)
 
     getShortestPath(source, target, path, distance)
 
