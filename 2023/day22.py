b8ab52480e0065db69fecc4b6e5194cac16f90b6
Brendon Forsgren <realpepino@gmail.com>
2023-12-22 22:03:40 -0600

    Can settle the bricks

--- 2023/day22.py
+++ 2023/day22.py
@@ -1,19 +1,66 @@
+import numpy as np
 
 def parseData(data):
     snapshot_locations = []
     for line in data:
         vals = line[:-1].split('~')
-        temp1 = (int(c) for c in vals[0].split(','))
-        temp2 = (int(c) for c in vals[1].split(','))
-        snapshot_locations.append((temp1, temp2))
+        temp1 = tuple(int(c) for c in vals[0].split(','))
+        temp2 = tuple(int(c) for c in vals[1].split(','))
+        snapshot_locations.append([temp1, temp2])
 
     return snapshot_locations
+
+def settleBricks(snapshot_locations):
+    max_x, max_y = 0, 0
+    max_z = 0
+    for brick in snapshot_locations:
+        edge1, edge2 = brick
+        x = max(edge1[0], edge2[0])+1
+        if x > max_x: max_x = x
+        y = max(edge1[1], edge2[1])+1
+        if y > max_y: max_y = y
+        delta_z = abs(edge1[2]- edge2[2])+1
+        max_z += delta_z
+
+    # Note: Np prints (page, row, col) or (z, y, x)
+    grid = np.zeros((max_z, max_y, max_x))
+    z_counter = 0
+    edge1, edge2 = snapshot_locations[0]
+    x1, y1, z1 = edge1
+    x2, y2, z2 = edge2
+    delta_z = z2 - z1 + 1
+    grid[z_counter:z_counter+delta_z, y1:y2+1, x1:x2+1] = 1
+    z_counter += delta_z
+
+    for i in range(1, len(snapshot_locations)):
+        brick = snapshot_locations[i]
+        edge1, edge2 = brick
+        x1, y1, z1 = edge1
+        x2, y2, z2 = edge2
+        delta_z = z2 - z1 + 1
+
+        # find first page where all values between x and y are zero
+        for j in range(max_z-1, -1, -1):
+            if np.any(grid[j, y1:y2+1, x1:x2+1] != 0):
+                z_counter = j+1
+                break
+
+        grid[z_counter:z_counter+delta_z, y1:y2+1, x1:x2+1] = i+1
+
+    return grid
+
 
 def part1():
     with open('temp.txt', 'r') as f:
         data = f.readlines()
 
     snapshot_locations = parseData(data)
+    settled_positions = settleBricks(snapshot_locations)
+    ## Get neighbors below
+    # neighbors = getNeighbors(settled_positions)  # return a dict
+    # Can remove supporting bricks with more than one
+    # Combine lists for bricks in a given row and see if they overlap
+    debug = 1
 
 def part2():
     pass
