14ebad89d2c8d09bc59c5a74d6b3d3de481909a8
Brendon Forsgren <realpepino@gmail.com>
2024-12-11 06:59:07 -0600

    Reworking part2 due to efficiency reasons. Have a plan using base mappings and recursion

--- 2024/day11/main.cpp
+++ 2024/day11/main.cpp
@@ -43,6 +43,37 @@ void part1(std::vector<uint64_t> data) {
     std::cout << "Part 1: " << data.size() << std::endl;
 }
 
+// Need some way to speed it up
+void part2(std::vector<uint64_t> data) {
+    // May need a table of partial mappings
+    std::map<uint64_t, std::vector<uint64_t>> baseMappings{
+        {0, std::vector<uint64_t>{1}},
+        {1, std::vector<uint64_t>{2, 0, 2, 4}},
+        {2, std::vector<uint64_t>{4, 0, 4, 8}},
+        {3, std::vector<uint64_t>{6, 0, 7, 2}},
+        {4, std::vector<uint64_t>{8, 0, 9, 6}},
+        {5, std::vector<uint64_t>{2, 0, 4, 8, 2, 8, 8, 0}},
+        {6, std::vector<uint64_t>{2, 4, 5, 7, 9, 4, 5, 6}},
+        {7, std::vector<uint64_t>{2, 8, 6, 7, 6, 0, 3, 2}},
+        {8, std::vector<uint64_t>{3, 2, 7, 7, 2, 6, 0, 8}},
+        {9, std::vector<uint64_t>{3, 6, 8, 6, 9, 1, 8, 4}},
+    };
+    std::map<uint64_t, int> numSteps{{0, 1}, {1, 3}, {2, 3}, {3, 3}, {4, 3}, {5, 5}, {6, 5}, {7, 5}, {8, 5}, {9, 5}};
+    std::vector<int> stepCnt(data.size(), 0);
+
+    std::vector<uint64_t> newData{};
+    for (int i{0}; i != data.size(); ++i) {
+        int stepCounter{0};
+        std::vector<uint64_t> temp{data[i]};
+        while (stepCounter != 75) {
+            // The code for this section
+            // Probably easiest with recursion
+        }
+    }
+
+    std::cout << "Part 2: " << data.size() << std::endl;
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         std::cout << "Input the path to the input file" << std::endl;
@@ -54,7 +85,7 @@ int main(int argc, char *argv[]) {
     parseData(input_file, data);
 
     part1(data);
-    // part2(data);
+    part2(data);
 
     return 0;
 }
