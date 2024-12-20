7f89458bbe8965b6bfa1efa339251e45d53daaac
Brendon Forsgren <realpepino@gmail.com>
2024-12-20 06:55:47 -0600

    Update to the bfs algorithm. the hard part is determining what cheats to actually check

--- 2024/day20/main.cpp
+++ 2024/day20/main.cpp
@@ -11,8 +11,8 @@
 #include <tuple>
 #include <vector>
 
-// constexpr int MINTIMESAVED = 1;
-constexpr int MINTIMESAVED = 100;
+constexpr int MINTIMESAVED = 50;
+// constexpr int MINTIMESAVED = 100;
 
 void parseData(const std::string &file, std::vector<std::string> &data) {
     std::ifstream fin{file};
@@ -103,6 +103,83 @@ void part1(const std::vector<std::string> &data) {
     }
 
     std::cout << "Part 1: " << cheatPoints.size() << std::endl;
+}
+
+bool savesTime2(std::vector<std::string> data, Point pt) {
+    auto start = pt.start;
+    auto end = pt.end;
+    size_t dist = abs(static_cast<int>(start.first) - static_cast<int>(end.first)) +
+                  abs(static_cast<int>(start.second) - static_cast<int>(end.second));
+
+    // BFS but keep track of distance
+    size_t row{start.first}, col{start.second}, cost{0};
+    std::vector<std::vector<bool>> visited(data.size(), std::vector<bool>(data[0].size(), false));
+    std::priority_queue<std::tuple<size_t, size_t, size_t>, std::vector<std::tuple<size_t, size_t, size_t>>, std::greater<std::tuple<size_t, size_t, size_t>>> stack{};
+    stack.push({0, row, col});
+    while (!stack.empty()) {
+        auto pair = stack.top();
+        stack.pop();
+        cost = std::get<0>(pair);
+        row = std::get<1>(pair);
+        col = std::get<2>(pair);
+
+        if (visited[row][col]) continue;
+        visited[row][col] = true;
+        data[row][col] = 'X';
+
+        // for (auto str : grid)
+        //     std::cout << str << "\n";
+        // std::cout << "============================\n"
+        //           << std::endl;
+
+        if (row == end.first && col == end.second) {  // Reached the end
+            // This will be the cost - the manhattan distance between start and end
+            if (cost - dist >= MINTIMESAVED)
+                return true;
+            else
+                return false;
+        }
+
+        // Push new nodes onto the stack
+        if (row > 0 && !visited[row - 1][col] && data[row - 1][col] != '#') {
+            stack.push({cost + 1, row - 1, col});
+        }
+        if (row < data.size() - 1 && !visited[row + 1][col] && data[row + 1][col] != '#') {
+            stack.push({cost + 1, row + 1, col});
+        }
+        if (col > 0 && !visited[row][col - 1] && data[row][col - 1] != '#') {
+            stack.push({cost + 1, row, col - 1});
+        }
+        if (col < data[0].size() - 1 && !visited[row][col + 1] && data[row][col + 1] != '#') {
+            stack.push({cost + 1, row, col + 1});
+        }
+    }
+
+    // Paths didn't meet up
+    return false;
+}
+
+// Cheat time of 20 picoseconds
+void part2(const std::vector<std::string> &data) {
+    // No need to check first/last row, col
+    std::set<Point> cheatPoints{};
+    for (size_t i{1}; i != data.size() - 1; ++i) {
+        for (size_t j{1}; j != data[i].size() - 1; ++j) {
+            if (data[i][j] == '#' && data[i][j + 1] != '#' && data[i][j - 1] != '#') {
+                const Point pt{std::make_pair(i, j - 1), std::make_pair(i, j + 1)};
+                if (savesTime2(data, pt)) {
+                    cheatPoints.insert(pt);
+                }
+            } else if (data[i][j] == '#' && data[i + 1][j] != '#' && data[i - 1][j] != '#') {
+                const Point pt{std::make_pair(i - 1, j), std::make_pair(i + 1, j)};
+                if (savesTime2(data, pt)) {
+                    cheatPoints.insert(pt);
+                }
+            }
+        }
+    }
+
+    std::cout << "Part 2: " << cheatPoints.size() << std::endl;
 }
 
 int main(int argc, char *argv[]) {
