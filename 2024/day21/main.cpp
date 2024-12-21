4cd5be88dd1c83451f0c724f4e78d22522441832
Brendon Forsgren <realpepino@gmail.com>
2024-12-21 10:41:54 -0600

    started part1 of day 21

---
+++ 2024/day21/main.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <vector>
+
+// constexpr int MINTIMESAVED = 50;
+constexpr int MINTIMESAVED = 100;
+
+void parseData(const std::string &file, std::vector<std::string> &data) {
+    std::ifstream fin{file};
+    if (!fin.is_open()) return;
+
+    std::string line;
+    while (std::getline(fin, line)) {
+        data.push_back(line);
+    }
+}
+
+void part1(const std::vector<std::string> &data) {
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cout << "Input the path to the input file" << std::endl;
+        return 0;
+    }
+
+    std::string input_file = std::string(argv[1]);
+    std::vector<std::string> data{};
+    parseData(input_file, data);
+
+    part1(data);
+
+    return 0;
+}
