fee50d3f598bc1468ebc1cfb668f803893d97d99
Brendon Forsgren <realpepino@gmail.com>
2024-12-24 11:17:34 -0600

    Going to attempt this later

--- 2024/day24/main.cpp
+++ 2024/day24/main.cpp
@@ -78,6 +78,58 @@ void part1(std::map<std::string, int> wireMap, const std::vector<std::string> &g
     std::cout << "Part 1: " << num << std::endl;
 }
 
+// Z should be an addition of x and y
+// Get expected bits for Z and compare to actual bits for Z
+// Which digits are off??
+void part2(std::map<std::string, int> wireMap, const std::vector<std::string> &gateMap) {
+    std::vector<bool> flags(gateMap.size(), false);
+    while (std::find(flags.begin(), flags.end(), false) != flags.end()) {
+        size_t cnt{0};
+        for (std::string str : gateMap) {
+            // Already performed this operation
+            if (flags[cnt]) {
+                ++cnt;
+                continue;
+            }
+            std::stringstream ss{str};
+            std::string wire1, wire2, wireOut, gate, temp;
+            std::getline(ss, wire1, ' ');
+            std::getline(ss, gate, ' ');
+            std::getline(ss, wire2, ' ');
+            std::getline(ss, temp, ' ');
+            std::getline(ss, wireOut, ' ');
+
+            // Can't do this one yet
+            if (wireMap.count(wire1) == 0 || wireMap.count(wire2) == 0) {
+                ++cnt;
+                continue;
+            }
+
+            if (gate == "AND") {
+                wireMap[wireOut] = (wireMap[wire1] == wireMap[wire2] && wireMap[wire1] == 1) ? 1 : 0;
+            } else if (gate == "OR") {
+                wireMap[wireOut] = (wireMap[wire1] == 1 || wireMap[wire2] == 1) ? 1 : 0;
+            } else if (gate == "XOR") {
+                wireMap[wireOut] = (wireMap[wire1] != wireMap[wire2]) ? 1 : 0;
+            } else {
+                std::cout << "ERROR: SHOULDN'T BE HERE" << std::endl;
+            }
+            flags[cnt] = true;
+            ++cnt;
+        }
+    }
+
+    // Compile the number
+    int64_t num{0};
+    for (auto it{wireMap.rbegin()}; it != wireMap.rend(); ++it) {
+        // std::cout << it->first << " " << it->second << std::endl;
+        if (it->first[0] != 'z') continue;
+        num = (num << 1) + it->second;
+    }
+
+    std::cout << "Part 2: " << num << std::endl;
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         std::cout << "Input the path to the input file" << std::endl;
@@ -90,6 +142,7 @@ int main(int argc, char *argv[]) {
     parseData(input_file, wireMap, gateMap);
 
     part1(wireMap, gateMap);
+    part2(wireMap, gateMap);
 
     return 0;
 }
