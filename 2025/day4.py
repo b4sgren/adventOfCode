22f60edcff60df9c01cbca999ca3902b1da4e744
Brendon Forsgren <forsgren@vectornav.com>
2025-12-04 09:05:16 -0600

    Part 1 mostl done. A few edge cases to work out

---
+++ 2025/day4.py
@@ -0,0 +1,49 @@
+
+def part1(file):
+    with open(file, 'r') as f:
+        data = f.readlines()
+    
+    # pad around to not worry about overflow/underflow
+    for i in range(len(data)):
+        # strip new line off
+        data[i] = '.' + data[i][:-1] + '.'
+
+    num_rows = len(data)
+    num_cols = len(data[0])
+    data.insert(0, '.'*(num_cols))
+    data.append('.' * (num_cols))
+
+
+    num_papers_to_move = 0
+    for i in range(1, num_rows):
+        for j in range(1, num_cols-1):
+            if data[i][j] != '@':
+                continue
+
+            num_surrounding_papers = 0
+            im1 = i-1
+            ip1 = i+1
+            jm1 = j-1
+            jp1 = j+1
+
+            num_surrounding_papers += 1 if data[im1][jm1] == '@' else 0
+            num_surrounding_papers += 1 if data[im1][j] == '@' else 0
+            num_surrounding_papers += 1 if data[im1][jp1] == '@' else 0
+            num_surrounding_papers += 1 if data[i][jm1] == '@' else 0
+            num_surrounding_papers += 1 if data[i][jp1] == '@' else 0
+            num_surrounding_papers += 1 if data[ip1][jm1] == '@' else 0
+            num_surrounding_papers += 1 if data[ip1][j] == '@' else 0
+            num_surrounding_papers += 1 if data[ip1][jp1] == '@' else 0
+
+            if num_surrounding_papers < 4:
+                num_papers_to_move += 1
+    
+    print(num_papers_to_move)
+
+
+if __name__=="__main__":
+    file = "test_input.txt"
+    # file = "input.txt" 
+
+    part1(file)
+    # part2(file)
