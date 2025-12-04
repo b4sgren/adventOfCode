bf1ae33b2731ed779fe61c7d2216dbee9c4d2d8c
Brendon Forsgren <forsgren@vectornav.com>
2025-12-04 09:45:17 -0600

    Day 2 is done

--- 2025/day4.py
+++ 2025/day4.py
@@ -16,6 +16,7 @@ def countNumPapersToRemove(data):
     num_rows = len(data)
 
 
+    ids = []
     num_papers_to_move = 0
     for i in range(1, num_rows):
         for j in range(1, num_cols):
@@ -39,8 +40,9 @@ def countNumPapersToRemove(data):
 
             if num_surrounding_papers < 4:
                 num_papers_to_move += 1
+                ids.append([i, j])
     
-    return num_papers_to_move
+    return num_papers_to_move, ids
 
 def part1(file):
     with open(file, 'r') as f:
@@ -48,7 +50,28 @@ def part1(file):
     
     # pad around to not worry about overflow/underflow
     data = padData(data)
-    num_papers_to_move = countNumPapersToRemove(data)
+    num_papers_to_move, _ = countNumPapersToRemove(data)
+    print(num_papers_to_move)
+
+def part2(file):
+    with open(file, 'r') as f:
+        data = f.readlines()
+    
+    data = padData(data)
+    # Convert to list for replacement
+    for i in range(len(data)):
+        data[i] = list(data[i])
+
+    num_papers_to_move = 0
+    temp_papers_to_move = 1
+    while temp_papers_to_move != 0:
+        temp_papers_to_move, ids = countNumPapersToRemove(data)
+        num_papers_to_move += temp_papers_to_move
+
+        # replace the data
+        for id in ids:
+            data[id[0]][id[1]] = '.'
+    
     print(num_papers_to_move)
 
 
@@ -57,4 +80,4 @@ if __name__=="__main__":
     file = "input.txt" 
 
     part1(file)
-    # part2(file)
+    part2(file)
