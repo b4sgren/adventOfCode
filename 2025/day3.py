af63125e11dea962b8e89e8432014005c5c59e4d
Brendon Forsgren <forsgren@vectornav.com>
2025-12-03 20:23:27 -0600

    Finished day3 part 2. input needs a new line at the end

--- 2025/day3.py
+++ 2025/day3.py
@@ -18,10 +18,39 @@ def part1(file):
     
     print(totalVoltage)
 
+# Requires Dynamic programming
+def getLargestVoltage2(line):
+    idx = -1
+    num = ''
+    # print("====================================")
+    for i in range(12, 0, -1):
+        # print(line[idx+1:-i])
+        if i > 1:
+            digit = max(line[idx+1:-i])
+        else:
+            digit = max(line[idx+1:])
+        idx = idx + 1 + line[idx+1:].index(digit)
+        num = num + digit
+    
+    return int(num)
+
+
+def part2(file):
+    with open(file, 'r') as f:
+        data = f.readlines()
+    
+    totalVoltage = 0
+    for line in data:
+        largestVoltage = getLargestVoltage2(line)
+        print(largestVoltage)
+        totalVoltage += largestVoltage
+    
+    print(totalVoltage)
+
 
 if __name__=="__main__":
     # file = "test_input.txt"
     file = "input.txt" 
 
     part1(file)
-    # part2(file)  
+    part2(file)  
