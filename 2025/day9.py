9218a26dc9974ee6e8686b36691656a4447a7e01
Brendon Forsgren <forsgren@vectornav.com>
2025-12-09 14:23:55 -0600

    Fixed. Needed to use floats instead of ints

---
+++ 2025/day9.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+def part1(file):
+    with open(file, 'r') as f:
+        data = f.readlines()
+    
+    corners = []
+    for line in data:
+        vals = line.split(',')
+        corners.append([float(vals[0]), float(vals[1])])
+    
+    corners = np.array(corners)
+    x = corners[:, 0]
+    y = corners[:, 1]
+
+    dx = np.abs(x - x[:, np.newaxis]) + 1
+    dy = np.abs(y - y[:, np.newaxis]) + 1
+
+    area = dx * dy
+
+    print(np.max(np.abs(area)))
+
+    max_area = 0
+    for i in range(len(corners)-1):
+        for j in range(i+1, len(corners)):
+            x1 = x[i]
+            x2 = x[j]
+            y1 = y[i]
+            y2 = y[j]
+            area = (abs(x1-x2)+1) * (abs(y1-y2)+1)
+            if area > max_area:
+                max_area = area
+    
+    print(max_area)
+
+
+if __name__=="__main__":
+    # file = "test_input.txt"
+    file = "input.txt" # 2147466380 is to low
+
+    part1(file)
+    # part2(file)
